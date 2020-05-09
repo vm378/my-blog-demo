f8b5547769fd26ece6154dd95832d5df58a297d3
Viswajith Mallampati <vm378@scarletmail.rutgers.edu>
2020-05-08 23:18:09 -0400

    Backend files

---
+++ src/build/precache-manifest.83e446b668f8ff61c37d48df670d55f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9e3b8bbba6628496f49b2733ba53b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3b84fc873384fc86c84",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b84fc873384fc86c84",
+    "url": "/static/js/main.b86eddc1.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
